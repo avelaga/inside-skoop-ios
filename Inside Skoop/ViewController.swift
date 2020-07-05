8350d31f883dd114b982fd934c92b9582e1c991c
Abhi Velaga <abhinav.velaga@gmail.com>
2020-07-05 13:41:36 -0500

    finished mains creen ui

--- Inside Skoop/ViewController.swift
+++ Inside Skoop/ViewController.swift
@@ -16,13 +16,63 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     let departments = ["All Departments", "A", "B", "C", "D", "E"]
     
+    // saved search parameters
+    var query = ""
+    var department = "All Departments"
+    var easyA = false
+    var goodProfessor = false
+    var lightHomework = false
+    var projectHeavy = false
+    var actuallyUseful = false
+    
     @IBOutlet weak var departmentField: UITextField!
     @IBOutlet weak var searchField: UITextField!
+    
+    @IBOutlet weak var easyAButton: UIButton!
+    @IBOutlet weak var goodProfessorButton: UIButton!
+    @IBOutlet weak var lightHomeworkButton: UIButton!
+    @IBOutlet weak var projectHeavyButton: UIButton!
+    @IBOutlet weak var actuallyUsefulButton: UIButton!
+    
+    @IBOutlet weak var searchButton: UIButton!
     
     let thePicker = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        thePicker.delegate = self
+        departmentField.inputView = thePicker
+        initUIStyles()
+        //        let ref = Database.database().reference(withPath: "courses")
+        
+        //        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")
+        //        guard let requestUrl = url else { fatalError() }
+        //        var request = URLRequest(url: requestUrl)
+        //        request.httpMethod = "GET"
+        //        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        //
+        //            // Check if Error took place
+        //            if let error = error {
+        //                print("Error took place \(error)")
+        //                return
+        //            }
+        //
+        //            // Read HTTP Response Status code
+        //            if let response = response as? HTTPURLResponse {
+        //                print("Response HTTP Status code: \(response.statusCode)")
+        //            }
+        //
+        //            // Convert HTTP Response Data to a simple String
+        //            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+        //                print("Response data string:\n \(dataString)")
+        //            }
+        //
+        //        }
+        //        task.resume()
+    }
+    
+    func initUIStyles(){
+//        let buttonRadius = 15.0
         searchField.layer.cornerRadius = 18.0
         searchField.layer.borderWidth = 1.0
         searchField.layer.borderColor = UIColor.lightGray.cgColor
@@ -32,37 +82,88 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         departmentField.text  = departments[0]
         departmentField.layer.borderColor = UIColor.white.cgColor
         
+        easyAButton.layer.cornerRadius = 18.0
+        goodProfessorButton.layer.cornerRadius = 18.0
+        lightHomeworkButton.layer.cornerRadius = 18.0
+        projectHeavyButton.layer.cornerRadius = 18.0
+        actuallyUsefulButton.layer.cornerRadius = 18.0
         
-        thePicker.delegate = self
-        departmentField.inputView = thePicker
+        easyAButton.layer.borderWidth = 1.0
+        goodProfessorButton.layer.borderWidth = 1.0
+        lightHomeworkButton.layer.borderWidth = 1.0
+        projectHeavyButton.layer.borderWidth = 1.0
+        actuallyUsefulButton.layer.borderWidth = 1.0
         
-//        let ref = Database.database().reference(withPath: "courses")
-//        print(ref)
+        setButtonAsUnselected(button: easyAButton)
+        setButtonAsUnselected(button: goodProfessorButton)
+        setButtonAsUnselected(button: lightHomeworkButton)
+        setButtonAsUnselected(button: projectHeavyButton)
+        setButtonAsUnselected(button: actuallyUsefulButton)
         
-        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")
-        guard let requestUrl = url else { fatalError() }
-        var request = URLRequest(url: requestUrl)
-        request.httpMethod = "GET"
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
-            // Check if Error took place
-            if let error = error {
-                print("Error took place \(error)")
-                return
-            }
-            
-            // Read HTTP Response Status code
-            if let response = response as? HTTPURLResponse {
-                print("Response HTTP Status code: \(response.statusCode)")
-            }
-            
-            // Convert HTTP Response Data to a simple String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
-            
+        searchButton.layer.borderColor = UIColor.lightGray.cgColor
+        searchButton.layer.cornerRadius = 23.0
+        searchButton.layer.borderWidth = 1.0
+    }
+    
+    func setButtonAsSelected(button: UIButton){
+        button.layer.borderColor = UIColor.clear.cgColor
+        button.backgroundColor = UIColor(red: 237/255, green: 125/255, blue: 121/255, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+    }
+    
+    func setButtonAsUnselected(button: UIButton){
+        button.setTitleColor(.black, for: .normal)
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.backgroundColor = UIColor.clear.cgColor
+    }
+    
+    @IBAction func easyAToggled(_ sender: Any) {
+        if easyA {
+            setButtonAsUnselected(button: easyAButton)
+        }else{
+            setButtonAsSelected(button: easyAButton)
         }
-        task.resume()
+        easyA = !easyA
+    }
+    
+    @IBAction func goodProfessorToggled(_ sender: Any) {
+        if goodProfessor {
+            setButtonAsUnselected(button: goodProfessorButton)
+        }else{
+            setButtonAsSelected(button: goodProfessorButton)
+        }
+        goodProfessor = !goodProfessor
+    }
+    
+    @IBAction func lightHomeworkToggle(_ sender: Any) {
+        if lightHomework {
+            setButtonAsUnselected(button: lightHomeworkButton)
+        }else{
+            setButtonAsSelected(button: lightHomeworkButton)
+        }
+        lightHomework = !lightHomework
+    }
+    
+    @IBAction func projectHeavyToggled(_ sender: Any) {
+        if projectHeavy {
+            setButtonAsUnselected(button: projectHeavyButton)
+        }else{
+            setButtonAsSelected(button: projectHeavyButton)
+        }
+        projectHeavy = !projectHeavy
+    }
+    
+    @IBAction func actuallyUsefulToggled(_ sender: Any) {
+        if actuallyUseful {
+            setButtonAsUnselected(button: actuallyUsefulButton)
+        }else{
+            setButtonAsSelected(button: actuallyUsefulButton)
+        }
+        actuallyUseful = !actuallyUseful
+    }
+    
+    @IBAction func searchPressed(_ sender: Any) {
+        performSegue(withIdentifier: "SearchSegueID", sender: nil)
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -79,7 +180,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        departmentField.text = departments[row]
+        department = departments[row]
+        departmentField.text = department
     }
     
     // code to enable tapping on the background to remove software keyboard
@@ -90,6 +192,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // make api call for search here with stored parameters and store result in destination
     }
     
 }
